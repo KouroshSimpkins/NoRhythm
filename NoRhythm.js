7d0d42922d0862bb188c1d643566d8d308e5f49a
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2021-12-20 00:05:37 +0000

    modified NoRhythm.js

--- NoRhythm.js
+++ NoRhythm.js
@@ -61,6 +61,10 @@ let type = "WebGL";
     PIXI.Loader.shared
       .add("assets/gun.png")
       .add("assets/projectile.png")
+      .add("assets/note-1.png")
+      .add("assets/note-2.png")
+      .add("assets/note-3.png")
+      .add("assets/note-4.png")
       .load(setup);
 
     let PlayerCharacter, state;
@@ -69,22 +73,38 @@ let type = "WebGL";
     var bulletSpeed = 5;
 
     function fireBullet(e) {
-      console.log("fire");
-
       let bullet = createBullet();
       bullets.push(bullet);
     }
 
     function createBullet() {
-      console.log("fire");
       let bullet = new PIXI.Sprite(resources["assets/projectile.png"].texture);
       bullet.anchor.set(0.5);
-      bullet.x = PlayerCharacter.x;
+      bullet.x = PlayerCharacter.x+10;
       bullet.y = PlayerCharacter.y;
       bullet.speed = bulletSpeed;
       app.stage.addChild(bullet);
 
       return bullet;
+    }
+
+    var notes = [];
+    var noteSpeed = 7;
+
+    function shootNote(e) {
+      let note = createNote();
+      notes.push(note);
+    }
+
+    function createNote() {
+      let note = new PIXI.Sprite(resources["assets/note-1.png"].texture);
+      note.anchor.set(0.5);
+      note.x = Math.random(0, app.renderer.width);
+      note.y = app.renderer.height;
+      note.speed = noteSpeed;
+      app.stage.addChild(note);
+
+      return note;
     }
 
     function setup() {
@@ -133,9 +153,35 @@ let type = "WebGL";
       app.ticker.add((delta) => gameloop(delta));
     }
 
+    function updateBullets(delta) {
+      for (let i = 0; i < bullets.length; i++) {
+        let bullet = bullets[i];
+        bullet.y -= bullet.speed * delta;
+
+        if (bullet.y < 0) {
+          app.stage.removeChild(bullet);
+          bullets.splice(i, 1);
+        }
+      }
+    }
+
+    function updateNotes(delta) {
+      for (let i = 0; i < notes.length; i++) {
+        let note = notes[i];
+        note.y += note.speed * delta;
+
+        if (note.y > app.renderer.height) {
+          app.stage.removeChild(note);
+          notes.splice(i, 1);
+        }
+      }
+    }
+
     function gameloop(delta) {
       // do stuff
       state(delta);
+      updateBullets(delta);
+      updateNotes(delta);
     }
 
     function play(delta) {
